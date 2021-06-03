6553e4b745adb4c4dd22328f22ba48f334b94fe8
Alex Berlaga <awb4@rice.edu>
2021-06-03 13:27:46 -0500

    fixed time increments

--- main.py
+++ main.py
@@ -1,53 +1,56 @@
 import random
+import asyncio
 import numpy as np
 import matplotlib.pyplot as plt
 
 N = 13
 U = 100
 W_VALUES = [10 * x for x in range(1, 25)]
-NUM_MCs = 1000
+NUM_MCs = 500
 TIME_INCR = .00025
 
-class MC_tube:
-    def __init__(self, n, u, w):
+class MC_tube_list:
+    def __init__(self, ntubes, n, u, w):
         """
         Sets parameters of a single microtubule
         :param n: number of protofilaments
         :param u: rate of changing a protofilament from state i to i-1.
         :param w: rate of changing a protofilament from state i to i+1.
         """
+        self.ntubes = ntubes
         self.n = n
         self.u = u * TIME_INCR
         self.w = w * TIME_INCR
         self.state = n
 
-    def simulate_mc(self):
-        """
-        Calculates the first passage time of the microtubule using Monte-Carlo
-        :return: first passage time of the microtubule to the catastrophe state (0). If the state is not reached
-        by the time MAX_TIME, returns 0.
-        """
-
-        if self.u + self.w > 1 or self.u < 0 or self.w < 0 or self.n < 2:
-            print("ERROR: parameters are not set correctly")
-
-        seconds = 0
-
-        while(True):
-
-            seconds += TIME_INCR
-            rand = random.random()
-            decrement_prob = self.state * self.u
-            increment_prob = (self.n - self.state) * self.w
-            reaction_prob = decrement_prob + increment_prob
-            if rand < decrement_prob:
-                self.state -= 1
-                if self.state == 0:
-                    return seconds
-                continue
-
-            elif rand < reaction_prob:
-                self.state += 1
+    # def simulate_mc(self):
+    #     """
+    #     Calculates the first passage time of the microtubule using Monte-Carlo
+    #     :return: first passage time of the microtubule to the catastrophe state (0). If the state is not reached
+    #     by the time MAX_TIME, returns 0.
+    #     """
+    #
+    #     if self.u + self.w > 1 or self.u < 0 or self.w < 0 or self.n < 2:
+    #         print("ERROR: parameters are not set correctly")
+    #
+    #     times = []
+    #     for i in range(self.ntubes):
+    #         seconds = 0
+    #         while True:
+    #             seconds += TIME_INCR
+    #             rand = random.random()
+    #             decrement_prob = self.state * self.u
+    #             increment_prob = (self.n - self.state) * self.w
+    #             reaction_prob = decrement_prob + increment_prob
+    #             if rand < decrement_prob:
+    #                 self.state -= 1
+    #                 if self.state == 0:
+    #                     times.append(seconds)
+    #                     break
+    #
+    #             elif rand < reaction_prob:
+    #                 self.state += 1
+    #     return times
 
     def simulate_gillespie(self):
         """
@@ -58,38 +61,46 @@ class MC_tube:
         if self.u + self.w > 1 or self.u < 0 or self.w < 0 or self.n < 2:
             print("ERROR: parameters are not set correctly")
 
-        time = 0
+        times = []
+        for i in range(self.ntubes):
+            time = 0
+            while(True):
+                decrement_prob = (self.state * self.u) * TIME_INCR
+                increment_prob = (self.n - self.state) * self.w * TIME_INCR
+                reaction_prob = decrement_prob + increment_prob
 
-        while(True):
+                rand_t = random.random()
+                rand_r = random.random()
 
-            decrement_prob = self.state * self.u
-            increment_prob = (self.n - self.state) * self.w
-            reaction_prob = decrement_prob + increment_prob
+                rxn_time = (1 / reaction_prob) * np.log(1 / rand_t) * TIME_INCR
+                time += rxn_time
 
-            rand_t = random.random()
-            rand_r = random.random()
+                if (rand_r < (decrement_prob / reaction_prob)):
+                    self.state -= 1
+                    if self.state == 0:
+                        times.append(time)
+                        break
+                else:
+                    self.state += 1
+        return times
 
-            rxn_time = (1 / reaction_prob) * np.log(1 / rand_t)
-            time += rxn_time
 
-            if (rand_r < (decrement_prob / reaction_prob)):
-                self.state -= 1
-            else:
-                self.state += 1
-
-            if self.state == 0:
-                return time
 
 
 
 def plot_simulation():
     avg_times = []
+    w_example = 50
+    mc = MC_tube_list(100000, N, U, w_example)
+    result = mc.simulate_gillespie()
+    result2 = [np.log10(x) for x in result]
+    plt.hist(result2, 400)
+    plt.yscale("log")
+    plt.show()
     for w in W_VALUES:
-        avg_time = 0
-        for i in range(NUM_MCs):
-            mc = MC_tube(N, U, w)
-            fp_time = mc.simulate_gillespie()
-            avg_time += fp_time / NUM_MCs
+        mc = MC_tube_list(NUM_MCs, N, U, w)
+        fp_times = mc.simulate_gillespie()
+        avg_time = np.average(fp_times)
         avg_times.append(avg_time)
         print(str(w) + ", " + str(avg_time))
     d = {"W value" : W_VALUES, "Average time" : avg_times}
