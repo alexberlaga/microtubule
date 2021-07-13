3579c9c3fd16038a18944ae95b56ccad9b1e7195
Alex Berlaga <awb4@rice.edu>
2021-07-13 13:30:16 -0500

    final code

--- microtubule-c.c
+++ microtubule-c.c
@@ -7,12 +7,12 @@
 #include "/Users/Alex/Downloads/mt19937-64/mt19937-64.c"
 
 #define N 13
-#define U_EXP 16
-#define W_EXP 20
-#define FWD_FINAL_STATE 0
+#define U_EXP 0.02
+#define W_EXP 0.02
+#define FWD_FINAL_STATE 4
 #define REV_FINAL_STATE 4
 
-#define NUM_MCS 100000
+#define NUM_MCS 40000
 
 // pthread_mutex_t mutex;
 
@@ -20,12 +20,9 @@
 struct MC_tube_list {
     int id;
     int ntubes;
-    int u;
-    int w;
+    double u;
+    double w;
     int cur;
-    
-   
-
 };
 
 /**
@@ -42,7 +39,7 @@ gus()
 }
 
 void *
-simulate_gillespie (void *ptr, int w_val, bool fwd, int dist_w_val) 
+simulate_gillespie (void *ptr, double w_val, bool fwd, double dist_w_val) 
 {
     (void) ptr;
     struct MC_tube_list mc = *(struct MC_tube_list *) ptr;
@@ -93,7 +90,7 @@ simulate_gillespie (void *ptr, int w_val, bool fwd, int dist_w_val)
     mc.w = w_val;
     mc.cur = 0;
 
-    printf("W = %d\n", w_val);
+    printf("W = %f\n", w_val);
 
     while (mc.cur < mc.ntubes) {
         time = 0;
@@ -127,7 +124,7 @@ simulate_gillespie (void *ptr, int w_val, bool fwd, int dist_w_val)
                 second_moment += (time * time / NUM_MCS);
                 mc.cur++;
                 // pthread_mutex_lock(&mutex);
-                if (mc.w == dist_w_val)
+                if (mc.w - dist_w_val < 0.0001 && mc.w - dist_w_val > -0.0001)
                     fprintf(result, "%f, ", time);
                 // pthread_mutex_unlock(&mutex);
                 if (!(mc.cur % 10000))
@@ -138,7 +135,7 @@ simulate_gillespie (void *ptr, int w_val, bool fwd, int dist_w_val)
     }
     variance = second_moment - (average_time * average_time);
     fano_factor = variance / (average_time * average_time);
-    fprintf(fano, "W = %d, MEAN: %f, VARIANCE: %f, FANO FACTOR: %f\n", mc.w, average_time, variance, fano_factor);
+    fprintf(fano, "W = %f, MEAN: %f, VARIANCE: %f, FANO FACTOR: %f\n", mc.w, average_time, variance, fano_factor);
     return (NULL);
 }
 
@@ -155,7 +152,7 @@ rng_tester(void *ptr)
     idum=gus();
     init_genrand64(idum);
 
-    for (i = 0; i < 10000; i++)
+    for (i = 0; i < 1000; i++)
         fprintf(rng, "%f\n", genrand64_real2());
     return (NULL);
 }
@@ -169,7 +166,7 @@ main(int argc, char **argv) {
     fopen("revresult.txt", "w");
     fopen("revfano.txt", "w");
     fopen("rng.txt", "w");
-    int w;
+    double w;
     // double w;
 
     // pthread_t tid[8];
@@ -188,6 +185,6 @@ main(int argc, char **argv) {
     //     pthread_join(tid[i], NULL);
 
     // pthread_mutex_destroy(&mutex);
-    for (w = 2; w < 50; w++)
-        simulate_gillespie(mcs, w, false, 20);
+    for (w = 0; w < 0.01; w+=0.001)
+        simulate_gillespie(mcs, w, true, 0.003);
 }
